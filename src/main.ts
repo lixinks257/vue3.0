91fad858678c9bffad5fcdb7aee375537b7fb8e7
xin.li <xin.li@quncaotech.com>
2022-05-02 11:36:10 +0800

    feat: 使用husky进行强制git代码提交规范

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@
 import { createApp } from 'vue'
 // 引入app组件（所有组件的父级组件）
 // import App from './App.vue'
-// import App from './App1.vue'
+import App from './App1.vue'
 // import App from './App2.vue'
 // import App from './App4.vue'
-import App from './App.vue'
+// import App from './App.vue'
 // 创建app应用返回对应的实例对象，调用mount方法进行挂载
 createApp(App).mount('#app')
